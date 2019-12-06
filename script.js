89d97d3a024d9f716710673f1a6492ac26ac41fb
Jan Lucca Doerrstein <doerrstein.lucca@yahoo.de>
2019-12-06 20:37:51 +0100

    changed heights, added video text grid, styles and faq styles plus text

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const acc = document.getElementsByClassName("accordion");
+function toggleNavbar() {
+  const navigationBar = document.getElementById("myTopnav");
+  if (navigationBar.className === "topnav") {
+    navigationBar.className += " responsive";
+  } else {
+    navigationBar.className = "topnav";
+  }
+} 
+
+const getAccordion = document.getElementsByClassName("accordion");
 const i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", () => {
+for (i = 0; i < getAccordion.length; i++) {
+  getAccordion[i].addEventListener("click", function() {
     this.classList.toggle("active");
     const panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
